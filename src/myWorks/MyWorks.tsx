c3e3c8a9353a979586f592e07361dec31e9ce6bf
asa121asa <crafta.net@gmail.com>
2021-02-06 20:32:30 +0300

    upd 4.1

--- src/myWorks/MyWorks.tsx
+++ src/myWorks/MyWorks.tsx
@@ -1,23 +1,27 @@
 import React from 'react';
-import s from './MyWorks.module.css'
+import s from './MyWorks.module.scss'
 import styles from './../comand/styles/Container.module.css'
 import MyWork from "./work/MyWork";
 import {Title} from "../comand/title/Title";
+import todo from  '../asses/image/todo.png'
 
 
 
 
 function MyWorks() {
+    const todolistImage = {
+        color: 'blue',
+        backgroundImage: `url(${todo})`,
+    };
+
     return(
-
-
             <div className={s.skillsBlock}>
                 <div className={`${styles.container} ${s.skillsContainer}`}>
                         <Title title={'Projects'}/>
 
                     <div className={s.skills}>
-                        <MyWork title={'CSS'} description={'SocialNetwork typeSkript hlgv klhlhvg klkgv lhgvlghv g vkhg v vk vkgv g vgvg '}/>
-                        <MyWork title={'REACT'} description={'Todolist TypeScript'}/>
+                        <MyWork style={todolistImage} title={'CSS'}  description={'SocialNetwork typeSkript hlgv klhlhvg klkgv lhgvlghv g vkhg v vk vkgv g vgvg '}/>
+                        <MyWork style={todolistImage} title={'REACT'} description={'Todolist TypeScript'}/>
                     </div>
                 </div>
 
--- src/myWorks/work/MyWork.tsx
+++ src/myWorks/work/MyWork.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import style from './MyWork.module.css'
+import style from './MyWork.module.scss'
 
 
-function MyWork(props: { title: string , description: string}) {
+function MyWork(props: { title: string , description: string, style: any}) {
     return (
 
 
         <div className={style.preject}>
 
-            <div className={style.image}>
+            <div className={style.image} style={props.style}>
                 <a className={style.btn}>show</a>
             </div>
 
