88cc970f2cbd8e525206ea221234cce4ec5835e8
asa121asa <crafta.net@gmail.com>
2021-02-16 18:57:04 +0300

    upd 4.3

--- src/myWorks/Projects.tsx
+++ src/myWorks/Projects.tsx
@@ -3,9 +3,7 @@ import s from './Projects.module.scss'
 import styles from './../comand/styles/Container.module.css'
 import MyWork from "./work/MyWork";
 import {Title} from "../comand/title/Title";
-import todo from  '../asses/image/todo.png'
-
-
+import todo from '../asses/image/todo.png'
 
 
 function Projects() {
@@ -14,22 +12,23 @@ function Projects() {
         backgroundImage: `url(${todo})`,
     };
 
-    return(
-            <div className={s.skillsBlock}>
-                <div className={`${styles.container} ${s.skillsContainer}`}>
-                        <Title title={'Projects'}/>
+    return (
+        <div className={s.skillsBlock}>
+            <div className={`${styles.container} ${s.skillsContainer}`}>
+                <Title title={'Projects'}/>
 
-                    <div className={s.skills}>
-                        <MyWork style={todolistImage} title={'CSS'}  description={'SocialNetwork typeSkript hlgv klhlhvg klkgv lhgvlghv g vkhg v vk vkgv g vgvg '}/>
-                        <MyWork style={todolistImage} title={'REACT'} description={'Todolist TypeScript'}/>
-                    </div>
+                <div className={s.skills}>
+                    <MyWork style={todolistImage} title={'CSS'}
+                            description={'SocialNetwork typeSkript hlgv klhlhvg klkgv lhgvlghv g vkhg v vk vkgv g vgvg '}/>
+                    <MyWork style={todolistImage} title={'REACT'} description={'Todolist TypeScript'}/>
                 </div>
-
             </div>
 
+        </div>
 
 
     )
 
 }
+
 export default Projects
--- src/skils/skill/Skill.tsx
+++ src/skils/skill/Skill.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import style from './Skill.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReact,  } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee, faHashtag} from '@fortawesome/free-solid-svg-icons'
 
 
-function Skill(props: { title: string , description: string}) {
+
+
+function Skill(props: { title: string , description: string, icon?: string}) {
     return (
 
 
         <div className={style.skill}>
-            <div className={style.icon}>icon</div>
+            <div className={style.icon}>{props.icon}</div>
+            <FontAwesomeIcon icon={['fab', 'apple']} />
+            <FontAwesomeIcon size={'7x'} icon={faHashtag} />
+            <i className="far fa-address-book"></i>
+            <FontAwesomeIcon  icon={"check-square"}/>
+
             <h3>{props.title}</h3>
             <span className={style.description}>
                 {props.description}
