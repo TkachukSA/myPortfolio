a50e88fdd3b0fea127ad751d8c16d4277004398a
asa121asa <crafta.net@gmail.com>
2021-02-20 01:54:19 +0300

    upd 4.4

--- src/remoteWork/RemoteWork.tsx
+++ src/remoteWork/RemoteWork.tsx
@@ -1,13 +1,14 @@
 import styles from './../comand/styles/Container.module.css'
 import s from './RemoteWork.module.css'
 import React from 'react';
+import {Title} from "../comand/title/Title";
 
 
 function RemoteWork() {
     return (
-        <div className={styles.container}>
+        <div className={`${styles.container} ${s.remoteContainer}`}>
             <div className={s.item}>
-            <h3>Рассматриваю варианты удаленной работы</h3>
+                <Title title={'Рассматриваю варианты удаленной работы'}/>
             <span>
                 <button>Нанять меня</button>
             </span>
