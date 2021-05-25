31915bb0c5fe1b725418231e50b4f0cb714f96a9
asa121asa <crafta.net@gmail.com>
2021-05-25 15:12:06 +0300

    add smtp server

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,18 @@
 import axios from 'axios'
+import {FormikErrorType} from "../contacts/Contacts";
 
+const instance = axios.create({
+    //baseURL: 'http://localhost:3010/',
+    baseURL: 'https://gmail-nodeks-server.herokuapp.com/',
+})
 
 
 export const api = {
     sendMessage(name: string,
                 message: string,
                 email: string) {
-        return axios.post('https://gmail-nodeks-server.herokuapp.com/sendMessge', {name, message, email});
+
+        return instance.post('sendMessge', {name, message,email});
     },
 
 }
